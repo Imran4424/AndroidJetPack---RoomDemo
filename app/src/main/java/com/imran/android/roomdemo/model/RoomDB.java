3f06d34f41abb5b607ee698d8acccc6ef8b5cad3
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-29 20:42:26 +0600

    model

--- app/src/main/java/com/imran/android/roomdemo/model/RoomDB.java
+++ app/src/main/java/com/imran/android/roomdemo/model/RoomDB.java
@@ -1,11 +1,34 @@
 package com.imran.android.roomdemo.model;
 
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
 /**
  * Created by Shah Md Imran Hossain on 29, November, 2020
  */
 
 // add database entities
+@Database(entities = {MainData.class}, version = 1, exportSchema = false)
+public class RoomDB extends RoomDatabase {
 
-public class RoomDB {
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
 
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
+    }
 }
