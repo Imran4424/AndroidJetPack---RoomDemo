105864ede35d48371d7088ca6a81b39065f7c11f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-30 01:23:45 +0600

    model

--- app/src/main/java/com/imran/android/roomdemo/model/MainDao.java
+++ app/src/main/java/com/imran/android/roomdemo/model/MainDao.java
@@ -1,6 +1,13 @@
 package com.imran.android.roomdemo.model;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+import static androidx.room.OnConflictStrategy.REPLACE;
 
 /**
  * Created by Shah Md Imran Hossain on 30, November, 2020
@@ -9,4 +16,13 @@ import androidx.room.Dao;
 @Dao
 public interface MainDao {
     // insert query
+    @Insert(onConflict = REPLACE)
+    void insert(MainData mainData);
+
+    // delete all query
+    @Delete
+    void reset(List<MainData> mainData);
+
+    // update query
+    @Query("UPDATE TextList SET text = ")
 }
