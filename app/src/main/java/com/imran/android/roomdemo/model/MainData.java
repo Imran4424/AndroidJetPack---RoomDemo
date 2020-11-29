81fb8f6cc2b4b51dca3464b153a8d2b2198b2a8b
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-29 17:36:56 +0600

    main data

--- app/src/main/java/com/imran/android/roomdemo/model/MainData.java
+++ app/src/main/java/com/imran/android/roomdemo/model/MainData.java
@@ -1,7 +1,23 @@
 package com.imran.android.roomdemo.model;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
 /**
  * Created by Shah Md Imran Hossain on 29, November, 2020
  */
-public class MainData {
+
+@Entity(tableName = "TextList")
+public class MainData implements Serializable {
+    // create id column
+    @PrimaryKey(autoGenerate = true)
+    private int ID;
+
+    @ColumnInfo(name = "text")
+    private String text;
+
+
 }
