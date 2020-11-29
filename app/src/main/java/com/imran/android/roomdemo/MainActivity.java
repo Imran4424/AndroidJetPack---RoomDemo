de739bd5589dec3be23715b371f9bc691bdca699
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-29 20:35:49 +0600

    model

--- app/src/main/java/com/imran/android/roomdemo/MainActivity.java
+++ app/src/main/java/com/imran/android/roomdemo/MainActivity.java
@@ -1,15 +1,24 @@
 package com.imran.android.roomdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.imran.android.roomdemo.model.MainData;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     EditText editTextInput;
     Button addButton;
     Button resetButton;
+
+    List<MainData> dataList = new ArrayList<>();
+    LinearLayoutManager linearLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/imran/android/roomdemo/model/RoomDB.java
@@ -0,0 +1,8 @@
+package com.imran.android.roomdemo.model;
+
+/**
+ * Created by Shah Md Imran Hossain on 29, November, 2020
+ */
+public class RoomDB {
+
+}
