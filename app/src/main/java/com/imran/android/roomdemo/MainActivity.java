690593c055696747144d916f0ee9d559124eab5f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-11-30 04:27:59 +0600

    model

--- app/src/main/java/com/imran/android/roomdemo/MainActivity.java
+++ app/src/main/java/com/imran/android/roomdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.imran.android.roomdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -17,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextInput;
     Button addButton;
     Button resetButton;
+    RecyclerView recyclerView;
 
     List<MainData> dataList = new ArrayList<>();
     RoomDB database;
@@ -31,5 +33,21 @@ public class MainActivity extends AppCompatActivity {
         editTextInput = findViewById(R.id.editTextInput);
         addButton = findViewById(R.id.addButton);
         resetButton = findViewById(R.id.resetButton);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // initialize database
+        database = RoomDB.getInstance(this);
+        // store database value in data list
+        dataList = database.mainDao().getAll();
+
+        // initialize linear layout manager
+        linearLayoutManager = new LinearLayoutManager(this);
+        // set layout manager
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        // initialize adapter
+        mainRecyclerAdapter = new MainRecyclerAdapter(this, dataList);
+        // set adapter
+        recyclerView.setAdapter(mainRecyclerAdapter);
     }
 }
